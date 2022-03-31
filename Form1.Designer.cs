0f3e67028556c9e4bf3925cac2cf4b59fa0c1f41
Tony Chan <tony3529353@gmail.com>
2022-03-31 18:29:44 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace IoTVendingMachineBackend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.item1 = new System.Windows.Forms.TextBox();
+            this.item4 = new System.Windows.Forms.TextBox();
+            this.item3 = new System.Windows.Forms.TextBox();
+            this.item2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.name1 = new System.Windows.Forms.Label();
+            this.name2 = new System.Windows.Forms.Label();
+            this.name3 = new System.Windows.Forms.Label();
+            this.name4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // item1
+            // 
+            this.item1.Location = new System.Drawing.Point(125, 72);
+            this.item1.Name = "item1";
+            this.item1.Size = new System.Drawing.Size(100, 25);
+            this.item1.TabIndex = 1;
+            // 
+            // item4
+            // 
+            this.item4.Location = new System.Drawing.Point(125, 227);
+            this.item4.Name = "item4";
+            this.item4.Size = new System.Drawing.Size(100, 25);
+            this.item4.TabIndex = 2;
+            // 
+            // item3
+            // 
+            this.item3.Location = new System.Drawing.Point(125, 175);
+            this.item3.Name = "item3";
+            this.item3.Size = new System.Drawing.Size(100, 25);
+            this.item3.TabIndex = 3;
+            // 
+            // item2
+            // 
+            this.item2.Location = new System.Drawing.Point(125, 122);
+            this.item2.Name = "item2";
+            this.item2.Size = new System.Drawing.Size(100, 25);
+            this.item2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(274, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Submit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(274, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Submit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // name1
+            // 
+            this.name1.AutoSize = true;
+            this.name1.Location = new System.Drawing.Point(21, 75);
+            this.name1.Name = "name1";
+            this.name1.Size = new System.Drawing.Size(44, 15);
+            this.name1.TabIndex = 9;
+            this.name1.Text = "name1";
+            // 
+            // name2
+            // 
+            this.name2.AutoSize = true;
+            this.name2.Location = new System.Drawing.Point(21, 125);
+            this.name2.Name = "name2";
+            this.name2.Size = new System.Drawing.Size(44, 15);
+            this.name2.TabIndex = 10;
+            this.name2.Text = "name2";
+            // 
+            // name3
+            // 
+            this.name3.AutoSize = true;
+            this.name3.Location = new System.Drawing.Point(21, 183);
+            this.name3.Name = "name3";
+            this.name3.Size = new System.Drawing.Size(44, 15);
+            this.name3.TabIndex = 11;
+            this.name3.Text = "name3";
+            // 
+            // name4
+            // 
+            this.name4.AutoSize = true;
+            this.name4.Location = new System.Drawing.Point(21, 230);
+            this.name4.Name = "name4";
+            this.name4.Size = new System.Drawing.Size(44, 15);
+            this.name4.TabIndex = 12;
+            this.name4.Text = "name4";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(140, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "UpdateStockToCloud";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 361);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.name4);
+            this.Controls.Add(this.name3);
+            this.Controls.Add(this.name2);
+            this.Controls.Add(this.name1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.item2);
+            this.Controls.Add(this.item3);
+            this.Controls.Add(this.item4);
+            this.Controls.Add(this.item1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox item1;
+        private System.Windows.Forms.TextBox item4;
+        private System.Windows.Forms.TextBox item3;
+        private System.Windows.Forms.TextBox item2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label name1;
+        private System.Windows.Forms.Label name2;
+        private System.Windows.Forms.Label name3;
+        private System.Windows.Forms.Label name4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IoTVendingMachineBackend
+{
+    public partial class Form1 : Form
+    {
+        // all lower case
+        string product1 = "chocolatebar";
+        string product2 = "popcorn";
+        string product3 = "twix";
+        string product4 = "gummybear";
+
+        int item1Num;
+        int item2Num;
+        int item3Num;
+        int item4Num;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static DeviceClient deviceClient;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int device = 1;  // using device 1 only as an example  (Tony Chan's device is number 1)
+            deviceClient = DeviceClient.CreateFromConnectionString(BuildConnectionString(device), TransportType.Mqtt); // connect to the cloud
+
+            name1.Text = product1;
+            name2.Text = product2;
+            name3.Text = product3;
+            name4.Text = product4;
+
+            ConnectToArduino();
+
+            // Initialize the stocks
+            // output: "itemName,Num"
+            // product1
+            Console.WriteLine(product1);
+            item1Num = Convert.ToInt32(Console.ReadLine()); // initialize the stock from seller (me)
+            string output1 = product1 + ',' + item1Num;
+            Console.WriteLine(output1);
+            port.Write(output1); // update the stock in IDE
+            port.ReadLine(); // skip the line we send
+            Console.WriteLine(" ");
+
+
+            // product2
+            Console.WriteLine(product2);
+            item2Num = Convert.ToInt32(Console.ReadLine());
+            string output2 = product2 + ',' + item2Num;
+            Console.WriteLine(output2);
+            port.Write(output2);
+            port.ReadLine();
+            port.Write(product2);
+            Console.WriteLine(" ");
+
+
+            // product3
+            Console.WriteLine(product3);
+            item3Num = Convert.ToInt32(Console.ReadLine());
+            string output3 = product3 + ',' + item3Num;
+            Console.WriteLine(output3);
+            port.Write(output3);
+            port.ReadLine();
+            port.Write(product3);
+            Console.WriteLine(" ");
+            
+
+            // product4
+            Console.WriteLine(product4);
+            item4Num = Convert.ToInt32(Console.ReadLine());
+            string output4 = product4 + ',' + item4Num;
+            Console.WriteLine(output4);
+            port.Write(output4);
+            port.ReadLine();
+            port.Write(product4);
+            Console.WriteLine(" ");
+
+            string msg = "Simulating device wchs00" + device.ToString() + ".  Ctrl-C to exit.\n";
+            Console.WriteLine(msg);
+
+
+            
+            // send the stock to the frontend first
+            item = new Product(product1, item1Num);
+            AdjustStock();
+            Thread.Sleep(500);
+
+            item = new Product(product2, item2Num);
+            AdjustStock();
+            Thread.Sleep(500);
+
+            item = new Product(product3, item3Num);
+            AdjustStock();
+            Thread.Sleep(500);
+
+            item = new Product(product4, item4Num);
+            AdjustStock();
+            
+
+
+        }
+
+
+        SerialPort port;
+        private void ConnectToArduino()
+        {
+            try
+            {
+                port = new SerialPort
+                {
+                    BaudRate = 9600,
+                    PortName = "COM11" // change the port if needed
+                };
+
+                port.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered error while reading serial port");
+                Console.WriteLine(ex.ToString());
+                System.Threading.Thread.Sleep(50000);
+            }
+        }
+
+        
+
+        private async Task GrabDataFromArduino() // Normal 
+        {
+            try
+            {
+                while (true)
+                {
+                    string dataFromArduino = port.ReadLine(); // blocking call so we have to use a new thread for this method
+                    dataFromArduino = dataFromArduino.Replace("\r", ""); // message includes "\r" in Seral buffer
+                    // incoming string: just itemName
+
+
+                    // *Do I really need to notify myslef for out of stock...? while the machine will literally be beside it..?
+                    Product normalItem = null;
+
+                    if (dataFromArduino == product1 && (item1Num != 0))
+                    {
+                        item1Num--; // decrease the num by one after we know a item is sell
+                        normalItem = new Product(dataFromArduino, item1Num);
+                    }
+
+                    if ((dataFromArduino == product2) && (item2Num != 0))
+                    {
+                        item2Num--;
+                        normalItem = new Product(dataFromArduino, item2Num);
+                    }
+
+                    if ((dataFromArduino == product3) && (item3Num != 0))
+                    {
+                        item3Num--;
+                        normalItem = new Product(dataFromArduino, item3Num);
+                    }
+
+                    if ((dataFromArduino == product4) && (item4Num != 0))
+                    {
+                        item4Num--;
+                        normalItem = new Product(dataFromArduino, item4Num);
+                    }
+
+
+                    if (normalItem != null)
+                    {
+                        string jsonData = JsonConvert.SerializeObject(normalItem);
+                        Microsoft.Azure.Devices.Client.Message message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(jsonData));
+
+
+                        // Send the JSON message to Azure 
+                        await deviceClient.SendEventAsync(message); // send the data to the cloud 
+
+
+                        // Console.WriteLine("{0:G} > Sent msg: {1}", DateTime.Now, jsonData.ToString()); // send the data to our console
+                        string displayMessage = "{0:G} > Sent msg: {1}" + DateTime.Now + jsonData.ToString();
+                        // listBox1.Items.Add(displayMessage);
+                        Console.WriteLine(displayMessage);
+
+                        await Task.Delay(5000);  // do not reduce this time interval (interval of each measurement)
+
+
+                        System.Threading.Thread.Sleep(500);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered error while reading serial port");
+                Console.WriteLine(ex.ToString());
+                System.Threading.Thread.Sleep(50000);
+            }
+
+            
+        }
+        private static string BuildConnectionString(int device)
+        {
+            string connectionString = "HostName=HellingaHub.azure-devices.net;DeviceId=wchs00";
+            connectionString += device.ToString(); // end with 001 in this case
+
+            switch (device)
+            {
+                case 1: connectionString += ";SharedAccessKey=Z6Vt7Trr5KBLV9CjPJ38vJlz4UIKKa3r8CUXNm8Ms+4="; break;
+                case 2: connectionString += ";SharedAccessKey=lyC8xfEoBrTTZVJ/9zvs111SJosH4fYCMiSzxIV+CPA="; break;
+                case 3: connectionString += ";SharedAccessKey=y/86Fww/H5buYweTeHX1Y0vxqdpSB6StopChZQ7bQPw="; break;
+                case 4: connectionString += ";SharedAccessKey=ufNV0f/4ZuvdImYRokL+UjZi/hHJMp5TL/V9brIBFX8="; break;
+                case 5: connectionString += ";SharedAccessKey=H2Dx6CQc9V+6cHyB1CY8MwkzY14I1HgyaXIfk8onmKQ="; break;
+                case 6: connectionString += ";SharedAccessKey=7bDraa8uywFjRhdysTZ/7TBeyYPy9ln/QwtLBYGIx5w="; break;
+            }
+            return connectionString;
+        }
+
+
+        Product item;
+        int num;
+        private async void AdjustStock()
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(item);
+
+                Microsoft.Azure.Devices.Client.Message message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(jsonData));
+
+                await deviceClient.SendEventAsync(message); // send the data to the cloud 
+
+                string displayMessage = "{0:G} > Sent msg: {1}" + DateTime.Now + jsonData.ToString();
+                Console.WriteLine(displayMessage);
+                port.ReadLine(); // skip the line we just send to Arduino
+
+                await Task.Delay(5000);  // do not reduce this time interval (interval of each measurement)
+                System.Threading.Thread.Sleep(500);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered error while reading serial port");
+                Console.WriteLine(ex.ToString());
+                System.Threading.Thread.Sleep(50000);
+            }
+
+        }
+
+        // adjust the number 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(item1.Text);
+            item = new Product(name1.Text.ToLower(), num);
+            port.Write(name1.Text + "," + num);  // Output: Chocolate,num: for adjustment in Arduino
+            AdjustStock(); // This method will skip the line we just write
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(item2.Text);
+            item = new Product(name2.Text.ToLower(), num);
+            port.Write(name2.Text+","+num);
+            AdjustStock();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(item3.Text);
+            item = new Product(name3.Text.ToLower(), num);
+            port.Write(name3.Text + "," + num);
+            AdjustStock();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(item4.Text);
+            item = new Product(name4.Text.ToLower(), num);
+            port.Write(name4.Text + "," + num);
+            AdjustStock();
+        }
+
+
+        // update the stock from arduino
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Visible = false;
+            // create a new thread that is used only to grab the data from arduino like some item is sold.
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                // Thread.CurrentThread.Join(); // main thead will never stop until the child thread is finished, which wont't happened in the while loop
+                /* run your code here */
+                // Console.WriteLine("Grabing data from Arduino...");
+                GrabDataFromArduino(); // keep sending the data
+            }).Start();
+
+            
+        }
+
+    }
+}
